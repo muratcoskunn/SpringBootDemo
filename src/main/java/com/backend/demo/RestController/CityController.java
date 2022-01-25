c2247d80952a28ae8feaec6f20d3c54a213602fb
muratcoskunn <97122845+muratcoskunn@users.noreply.github.com>
2022-01-25 23:18:20 +0300

    25.01.2022

--- src/main/java/com/backend/demo/RestController/CityController.java
+++ src/main/java/com/backend/demo/RestController/CityController.java
@@ -12,12 +12,12 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/city")
-public class CityController2 {
+public class CityController {
 
     ICityService cityService;
 
     @Autowired
-    public CityController2(ICityService cityService) {
+    public CityController(ICityService cityService) {
         this.cityService = cityService;
     }
 
