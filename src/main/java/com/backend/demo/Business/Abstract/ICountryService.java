f371401f21aab4c93aa30b377e20653fa5c19408
muratcoskunn <97122845+muratcoskunn@users.noreply.github.com>
2022-01-28 01:31:20 +0300

    28.01.2022

--- src/main/java/com/backend/demo/Business/Abstract/ICountryService.java
+++
@@ -1,9 +0,0 @@
-package com.backend.demo.Business.Abstract;
-
-import com.backend.demo.Entities.Country;
-
-import java.util.List;
-
-public interface ICountryService {
-    List<Country> getAll();
-}
--- src/main/java/com/backend/demo/Dal/ICountryDal.java
+++
@@ -1,7 +0,0 @@
-package com.backend.demo.Dal;
-
-import com.backend.demo.Entities.Country;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ICountryDal extends JpaRepository<Country,String> {
-}
